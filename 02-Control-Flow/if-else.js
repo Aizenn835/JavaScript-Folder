3e349d968181511a7152ae70b3ec76dd8af3dece
Lei Justine <clemente.leijustine@gmail.com>
2026-05-02 11:21:45 +0800

    Adding new learning

---
+++ 02-Control-Flow/if-else.js
@@ -0,0 +1,10 @@
+let name = prompt("Please Enter your name: ");
+let age = parseInt(prompt("Please enter your age: "));
+
+
+if(age > 18){
+    console.log(`Your an adult you can enter to a bar.`);
+}
+else{
+    console.log(`You cant enter.`);
+}
