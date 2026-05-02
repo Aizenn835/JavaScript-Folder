7a3336b01326c5e598a414725c473802fcc6bca5
Lei Justine <clemente.leijustine@gmail.com>
2026-05-02 11:48:38 +0800

    Adding for loop

---
+++ 02-Control-Flow/for-loop.js
@@ -0,0 +1,12 @@
+let = message = " ";
+for(let i = 0; i < 10; i++){
+    message += i + " ";
+}
+console.log(message);
+
+let getColors = "";
+let colors = ['red' , 'blue' , 'yellow'];
+for(let color of colors){
+    getColors += color + " ";
+}
+console.log(getColors);
